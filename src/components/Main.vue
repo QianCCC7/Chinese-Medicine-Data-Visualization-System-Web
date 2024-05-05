4598995eef91ee86c21c796ab6758af372500af1
xiaoqian <1805521303@qq.com>
2024-05-05 10:52:20 +0800

    前端关系图图例的实现

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -18,132 +18,132 @@ export default {
           name: '老大',
           symbolSize: 120,
           draggable: true,// 节点移动效果
-          category: 0,
+          category: '分类1',
           itemStyle: {
             normal: {
-              borderColor: 'rgb(213,143,154)',
+              // borderColor: '#82dffe',
               borderWidth: 6,
               shadowBlur: 20,
-              shadowColor: 'pink',
-              color: 'pink',
+              // shadowColor: 'pink',
+              // color: 'pink',
             }
           },
         },
         {
           name: '老二',
           symbolSize: 100,
-          category: 1,
+          category: '分类2',
           itemStyle: {
             normal: {
-              borderColor: '#04f2a7',
+              // borderColor: '#04f2a7',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43',
+              // shadowColor: '#04f2a7',
+              // color: '#001c43',
             }
           },
         }, {
           name: '老三',
           symbolSize: 80,
-          category: 1,
+          category: '分类3',
           itemStyle: {
             normal: {
-              borderColor: '#04f2a7',
+              // borderColor: '#04f2a7',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43',
+              // shadowColor: '#04f2a7',
+              // color: '#001c43',
             }
           },
         }, {
           name: '小蓝',
           symbolSize: 80,
-          category: 1,
+          category: '分类1',
           itemStyle: {
             normal: {
-              borderColor: '#82dffe',
+              // borderColor: '#82dffe',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43',
+              // shadowColor: '#04f2a7',
+              // color: '#001c43',
             }
           },
         }, {
           name: '小红',
           symbolSize: 80,
-          category: 1,
+          category: '分类2',
           itemStyle: {
             normal: {
-              borderColor: '#82dffe',
+              // borderColor: '#82dffe',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43',
+              // shadowColor: '#04f2a7',
+              // color: '#001c43',
             }
           },
         }, {
           name: '小绿',
           symbolSize: 100,
-          category: 2,
+          category: '分类2',
           itemStyle: {
             normal: {
-              borderColor: '#82dffe',
+              // borderColor: '#82dffe',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43',
+              // shadowColor: '#04f2a7',
+              // color: '#001c43',
             }
           },
         }, {
           name: '小青',
           symbolSize: 80,
-          category: 2,
+          category: '分类1',
           itemStyle: {
             normal: {
-              borderColor: '#b457ff',
+              // borderColor: '#b457ff',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#b457ff',
-              color: '#001c43'
+              // shadowColor: '#b457ff',
+              // color: '#001c43'
             }
           },
         }, {
           name: '小紫',
           symbolSize: 80,
+          category: '分类1',
           itemStyle: {
             normal: {
-              borderColor: '#82dffe',
+              // borderColor: '#82dffe',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43'
+              // shadowColor: '#04f2a7',
+              // color: '#001c43'
             }
           },
-          category: 2,
         }, {
           name: '小黑',
           symbolSize: 80,
+          category: '分类2',
           itemStyle: {
             normal: {
-              borderColor: '#82dffe',
+              // borderColor: '#82dffe',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43'
+              // shadowColor: '#04f2a7',
+              // color: '#001c43'
             }
           },
-          category: 2,
         }, {
           name: '小白',
           symbolSize: 80,
-          category: 2,
+          category: '分类2',
           itemStyle: {
             normal: {
-              borderColor: '#82dffe',
+              // borderColor: '#82dffe',
               borderWidth: 4,
               shadowBlur: 10,
-              shadowColor: '#04f2a7',
-              color: '#001c43'
+              // shadowColor: '#04f2a7',
+              // color: '#001c43'
             }
           },
         },
@@ -199,7 +199,7 @@ export default {
         type: 'graph',
         layout: 'force',
         force: {
-          repulsion: 1000, // 点和点间点距离
+          repulsion: 1000, // 点与点之间点距离
           edgeLength: 100 // 边的长度
         },
         roam: true, // 允许拖动图表移动
@@ -210,11 +210,18 @@ export default {
             color: "white"
           },
           emphasis: { // 高亮样式
-            color: "red"
+            // color: "black"
           }
         },
         data: this.map,
         links: this.relations,
+        categories: [{
+          name: '分类1'
+        }, {
+          name: '分类2'
+        }, {
+          name: '分类3'
+        }],
         // 设置边的样式
         lineStyle: {
           normal: {
@@ -230,7 +237,17 @@ export default {
             return params.data.value
           }
         },
-      }]
+      }],
+      legend: {
+        x: 'right',
+        data: ['分类1', '分类2', '分类3'], // 关系图中，对应的图例名称要和 series中 categories的 name属性保持一致
+        textStyle: {
+          color: "white",
+          fontSize: "16px"
+        },
+        orient: "vertical"
+      },
+      color: ['#3979d2', '#b457ff', '#82dffe'], // 如果 series没有设置颜色，则会依次循环从该列表中取颜色作为系列颜色
     }
     myEcharts.setOption(option)
   }
